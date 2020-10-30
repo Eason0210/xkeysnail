94f196dca01b3cce779f69b319ada9f04dc2d963
Eason0210 <aqua0210@163.com>
2020-10-31 04:58:59 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# xkeysnail-emacs
+Configuration for xkeysnail wiht Emacs-like Keybinding.
+
+## Install xkeysnail for offical repository.
+```bash
+git clone --depth 1 https://github.com/mooz/xkeysnail.git
+```
+## download this repository
+
+```bash
+https://github.com/Eason0210/xkeysnail-emacs.git
+```
+
+## Usage
+```
+sudo xkeysnail config.py
+```
+## Setting for automatic start
+1. Edit the config file and replace `Your-terminal-app-here` with the window class of your terminal application.
+(To get the window class name, run `xprop WM_CLASS` and click on the window of the terminal application)
+
+2. Create a systemd service for xkeysnail to run automatically on the background:
+
+```
+cd /etc/systemd/system && sudo vim xkeysnail.service
+```
+3. Insert the following code to the service config and edit the path to the provided config file:
+```
+sudo vim /etc/systemd/system/xkeysnail.service
+```
+
+```
+# https://qiita.com/samurai20000@github/items/2e1d779e806a7e8543d6
+# https://github.com/mooz/xkeysnail/issues/12
+
+[Unit]
+Description=xkeysnail
+
+[Service]
+Environment=DISPLAY=:0
+ExecStart=xkeysnail -q /home/aqua/.config/xkeysnail/config.py
+Restart=always
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+```
+4. write `xhost +SI:localuser:root` to ~/.xsession , restart system.
+
+5. Enable the xkeysnail service to auto start:
+```
+sudo systemctl enable xkeysnail
+```
+6. Start the xkeysnail service:
+```
+sudo systemctl start xkeysnail
+```
+7. If you want to edit config.py again, remember to restart service again.
+```
+sudo systemctl restart xkeysnail
+```
+## troubleshooting
+To check if the xkeysnail service is running properly, run:
+```
+sudo systemctl status xkeysnail
+```
+If you encounter errors like `Xlib.error.DisplayConnectionError: Can't connect to display ":0.0": b'No protocol specified\n'`, make sure you have xhost package installed and try:
+```
+sudo xhost + && sudo systemctl restart xkeysnail
+```
---
+++ config.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+
+import re
+from xkeysnail.transform import *
+
+# define timeout for multipurpose_modmap
+define_timeout(1)
+
+# [Global modemap] Change modifier keys as in xmodmap
+# define_modmap({
+#     Key.CAPSLOCK: Key.LEFT_CTRL
+# })
+
+# [Conditional modmap] Change modifier keys in certain applications
+# define_conditional_modmap(re.compile(r'Emacs'), {
+#     Key.RIGHT_CTRL: Key.ESC,
+# })
+
+# [Multipurpose modmap] Give a key two meanings. A normal key when pressed and
+# released, and a modifier key when held down with another key. See Xcape,
+# Carabiner and caps2esc for ideas and concept.
+# define_multipurpose_modmap(
+#     # Enter is enter when pressed and released. Control when held down.
+#     {Key.ENTER: [Key.ENTER, Key.RIGHT_CTRL]}
+
+#     # Capslock is escape when pressed and released. Control when held down.
+#     # {Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL]
+#     # To use this example, you can't remap capslock with define_modmap.
+# )
+
+# [Conditional multipurpose modmap] Multipurpose modmap in certain conditions,
+# such as for a particular device.
+# define_conditional_multipurpose_modmap(lambda wm_class, device_name: device_name.startswith("Microsoft"), {
+#     # Left shift is open paren when pressed and released.
+#     # Left shift when held down.
+#    Key.LEFT_SHIFT: [Key.KPLEFTPAREN, Key.LEFT_SHIFT],
+
+#    # Right shift is close paren when pressed and released.
+#     # Right shift when held down.
+#    Key.RIGHT_SHIFT: [Key.KPRIGHTPAREN, Key.RIGHT_SHIFT]
+# })
+# Keybindings for Firefox/Chromium
+
+define_keymap(re.compile("konsole"), {
+    # Type C-j to focus to the content
+    K("C-y"): K("C-Shift-v"),
+    K("M-w"): K("C-Shift-c"),
+}, "konsole")
+
+
+# Keybindings for Firefox/Chromium
+define_keymap(re.compile("Firefox|Chromium"), {
+    # Ctrl+Alt+j/k to switch next/previous tab
+    K("C-M-j"): K("C-TAB"),
+    K("C-M-k"): K("C-Shift-TAB"),
+    # Type C-j to focus to the content
+    K("C-j"): K("C-f6"),
+    # very naive "Edit in editor" feature (just an example)
+    # K("C-o"): [K("C-a"), K("C-c"), launch(["vim"]), sleep(0.5), K("C-v")]
+}, "Firefox and Chromium")
+
+# Keybindings for Zeal https://github.com/zealdocs/zeal/
+define_keymap(re.compile("Zeal"), {
+    # Ctrl+s to focus search area
+    K("C-s"): K("C-k"),
+}, "Zeal")
+
+# Emacs-like keybindings in non-Emacs applications
+define_keymap(lambda wm_class: wm_class not in ("Emacs", "konsole"), {
+    # Cursor
+    K("C-b"): with_mark(K("left")),
+    K("C-f"): with_mark(K("right")),
+    K("C-p"): with_mark(K("up")),
+    K("C-n"): with_mark(K("down")),
+    K("C-h"): with_mark(K("backspace")),
+    # Forward/Backward word
+    K("M-b"): with_mark(K("C-left")),
+    K("M-f"): with_mark(K("C-right")),
+    # Beginning/End of line
+    K("C-a"): with_mark(K("home")),
+    K("C-e"): with_mark(K("end")),
+    # Page up/down
+    K("M-v"): with_mark(K("page_up")),
+    K("C-v"): with_mark(K("page_down")),
+    # Beginning/End of file
+    K("M-Shift-comma"): with_mark(K("C-home")),
+    K("M-Shift-dot"): with_mark(K("C-end")),
+    # Newline
+    K("C-m"): K("enter"),
+    K("C-j"): K("enter"),
+    K("C-l"): [K("end"), K("enter")],
+    K("C-o"): [K("home"),K("enter"), K("up")],
+    # Copy
+    K("C-w"): [K("C-x"), set_mark(False)],
+    K("M-w"): [K("C-c"), set_mark(False)],
+    K("C-y"): [K("C-v"), set_mark(False)],
+    # Delete
+    K("C-d"): [K("delete"), set_mark(False)],
+    K("M-d"): [K("C-delete"), set_mark(False)],
+    # Kill line
+    K("C-k"): [K("Shift-end"), K("C-x"), set_mark(False)],
+    # Undo
+    K("C-slash"): [K("C-z"), set_mark(False)],
+    K("C-Shift-ro"): K("C-z"),
+    # Mark
+    K("M-h"): set_mark(True),
+    K("C-M-space"): with_or_set_mark(K("C-right")),
+    # Search
+    K("C-s"): K("F3"),
+    K("C-r"): K("Shift-F3"),
+    K("M-Shift-key_5"): K("C-h"),
+    # Cancel
+    K("C-g"): [K("esc"), set_mark(False)],
+    # Escape
+    K("C-q"): escape_next_key,
+    # C-x YYY
+    K("C-x"): {
+        # C-x h (select all)
+        K("h"): [K("C-home"), K("C-a"), set_mark(True)],
+        # C-x C-f (open)
+        K("C-f"): K("C-o"),
+        # C-x C-s (save)
+        K("C-s"): K("C-s"),
+        # C-x k (kill tab)
+        K("k"): K("C-f4"),
+        # C-x C-c (exit)
+        K("C-c"): K("C-q"),
+        # cancel
+        K("C-g"): pass_through_key,
+        # C-x u (undo)
+        K("u"): [K("C-z"), set_mark(False)],
+    },
+
+    # C-z prefix
+    K("C-z"): {
+        # C-z j (Beginning/End of file)
+        K("k"): with_mark(K("C-home")),
+        K("j"): with_mark(K("C-end")),
+    },
+}, "Emacs-like keys")
